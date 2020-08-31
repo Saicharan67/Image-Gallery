65a9abf841822a3696d626a4fd19d021079f3dcc
saicharan67 <mahankalisaicharan@gmail.com>
2020-08-31 21:27:23 +0530

    Added del button

--- src/components/gallery/gallery.js
+++ src/components/gallery/gallery.js
@@ -5,9 +5,22 @@ import './style.css'
 class Gallery extends React.Component{
 constructor(props){
     super(props);
-    this.state={
-        images:[],
-        currentAddress:"",
+    
+    this.rememberMe =  JSON.parse(localStorage.getItem("IMGS"));
+    if (this.rememberMe){
+        this.state={
+            images:this.rememberMe,
+            currentAddress:"",
+            count:this.rememberMe.length
+        }
+    }
+    else{
+        this.state={
+            images:[],
+            currentAddress:"",
+            count:0
+
+        }
     }
 }
 onaddressChange= (event) => {
@@ -15,25 +28,31 @@ onaddressChange= (event) => {
       currentAddress:event.target.value
   })
 }
-componentDidMount() {
-    const rememberMe = JSON.parse(localStorage.getItem("IMGS"));
-    
-    this.setState({ 
-        images: rememberMe });
-  }
+
 onaddimage= (event) =>{
     const n = [...this.state.images]
-
+    const vir = this.state.count
     n.push(this.state.currentAddress)
     localStorage.setItem("IMGS" , JSON.stringify(n));
     this.setState({
           images:n,
           currentAddress:"",
+          count:vir+1
 
     })
    
 }
+Ondelete = (ide) =>{
+    let vt = [...this.state.images]
+    console.log(ide)
+    vt.splice(ide,1)
+    this.setState({
+       images:vt
+    });
+      
+      localStorage.setItem("IMGS" , JSON.stringify(vt));
 
+}
 render = () => {
 
     return(
@@ -49,11 +68,12 @@ render = () => {
           </div>
           <div className="cards">
                {
-               this.state.images.map((b)=>{
-                 return <ImageCard 
-                 key={`${b}-${Math.random() * 10}`}
+               this.state.images.map((b,index)=>{
+                 return < ImageCard 
+                 key={Math.random()*100}
+                 id={index}
                  currentAddress={b} 
-                 
+                 WhenClicked={this.Ondelete}
                   
                  />
                })
--- src/components/imagecards/image.js
+++ src/components/imagecards/image.js
@@ -1,9 +1,18 @@
 import React from 'react'
 import './style.css'
 const ImageCard = (props) =>{
+    const myfucn = (event) =>{
+        props.WhenClicked(props.id)
+    }
 
     return (
-        <img  src={props.currentAddress}/>
+       <div>
+        <img  src={props.currentAddress}
+        
+        
+        />
+        <button  className="btn" value ={props.id} onClick={myfucn}>Remove</button>
+        </div>
     )
 }
 export default ImageCard
